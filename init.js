051f8e1b50705569dea79e8456b989e2de51764d
zikirillayev <91782186+zikirillayev@users.noreply.github.com>
2021-11-30 22:30:34 +0500

    Add files via upload

---
+++ init.js
@@ -0,0 +1,20 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+M.AutoInit();
+
+
+function translateX(){
+  let y = document.getElementById('btn');
+ document.querySelector('.simple').className = 'simple active'
+
+}
+function translateY(){
+  let z = document.getElementById('hbtn');
+   document.querySelector('.simple').className = 'simple'
+   
+}
